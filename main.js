9483712596653d3b63d0fa1edec0e00e47035a85
karlmorand <karljmorand@gmail.com>
2016-05-15 22:08:15 -0400

    added javascript to create the cards on the board

--- main.js
+++ main.js
@@ -5,8 +5,22 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour){
+/* if (cardTwo === cardFour){
 	alert("You found a match!");
 } else {
 	alert("Sorry try again.");
+} */
+
+var gameBoard = document.getElementById('game-board');
+
+
+
+var createBoard = function (){
+	for (i = 0; i < 4; i++){
+	var newCard = document.createElement('div');
+	newCard.className = 'card';
+	document.getElementById('game-board').appendChild(newCard);
 }
+}
+
+createBoard();
