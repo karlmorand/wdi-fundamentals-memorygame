e474d68afa9f6b97032af2ebe316ad54eb08e3bc
karlmorand <karljmorand@gmail.com>
2016-05-16 00:07:43 -0400

    finished game logic

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-/* if (cardTwo === cardFour){
-	alert("You found a match!");
-} else {
-	alert("Sorry try again.");
-} */
 
 var gameBoard = document.getElementById('game-board');
 
 
 
 var createBoard = function (){
-	for (i = 0; i < 4; i++){
+	for (i = 0; i < cards.length; i++){
 	var newCard = document.createElement('div');
 	newCard.className = 'card';
+	newCard.setAttribute('data-card', cards[i]);
+	newCard.addEventListener('click', isTwoCards);
+
 	document.getElementById('game-board').appendChild(newCard);
 }
 }
 
+var isMatch = function (cards) {
+	 if (cards[0] === cards[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry try again.");
+	}
+}
+
+var isTwoCards = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
 createBoard();
+
+
+
